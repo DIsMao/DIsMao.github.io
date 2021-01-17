71253646950636ca607dcc4722760b3d1b7e7749
DIsMao <74967959+DIsMao@users.noreply.github.com>
2021-01-17 19:47:58 +0300

    qwe
    qwe

---
+++ Avtr/vendor/connect.php
@@ -0,0 +1,7 @@
+<?php
+
+    $connect = mysqli_connect('localhost', 'id15940179_dismao', '0RzN~?>LYI}bLBTR', 'id15940179_site');
+
+    if (!$connect) {
+        die('Error connect to DataBase');
+    }
