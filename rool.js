adffeb05a941412871cdd8cc8e1df7a0e32438c0
DIsMao <74967959+DIsMao@users.noreply.github.com>
2021-01-23 00:49:16 +0300

    ///

---
+++ rool.js
@@ -0,0 +1,72 @@
+function out(){
+
+
+document.getElementById('out').style.width='60%'
+document.getElementById('out').style.height='40%'
+document.getElementById('out').style.top='30%'
+document.getElementById('out').style.left='20%'
+document.getElementById('out').style.zIndex='2'
+
+document.getElementById('out3').style.width='50%'
+document.getElementById('out3').style.height='30%'
+document.getElementById('out3').style.top='35%'
+document.getElementById('out3').style.left='45%'
+document.getElementById('out3').style.zIndex='1'
+
+document.getElementById('out2').style.width='50%'
+document.getElementById('out2').style.height='30%'
+document.getElementById('out2').style.top='35%'
+document.getElementById('out2').style.left='100%'
+document.getElementById('out2').style.zIndex='1'
+
+}
+function out2(){
+
+
+    document.getElementById('out2').style.width='60%'
+    document.getElementById('out2').style.height='40%'
+    document.getElementById('out2').style.top='30%'
+    document.getElementById('out2').style.left='20%'
+    document.getElementById('out2').style.zIndex='2'
+    
+    document.getElementById('out3').style.width='50%'
+    document.getElementById('out3').style.height='30%'
+    document.getElementById('out3').style.top='35%'
+    document.getElementById('out3').style.left='5%'
+    document.getElementById('out3').style.zIndex='1'
+    
+    document.getElementById('out').style.width='50%'
+    document.getElementById('out').style.height='30%'
+    document.getElementById('out').style.top='35%'
+    document.getElementById('out').style.left='-50%'
+    document.getElementById('out').style.zIndex='1'
+    
+    }
+    function out3(){
+
+
+        document.getElementById('out3').style.width='60%'
+        document.getElementById('out3').style.height='40%'
+        document.getElementById('out3').style.top='30%'
+        document.getElementById('out3').style.left='20%'
+        document.getElementById('out3').style.zIndex='2'
+        
+        document.getElementById('out2').style.width='50%'
+        document.getElementById('out2').style.height='30%'
+        document.getElementById('out2').style.top='35%'
+        document.getElementById('out2').style.left='45%'
+        document.getElementById('out2').style.zIndex='1'
+        
+        document.getElementById('out').style.width='50%'
+        document.getElementById('out').style.height='30%'
+        document.getElementById('out').style.top='35%'
+        document.getElementById('out').style.left='5%'
+        document.getElementById('out').style.zIndex='1'
+        
+        }
+//var content1;
+///var content2;
+
+//content1=document.getElementById('out');
+//content2=document.getElementById('out');
+//content1.outerHTML='<div class="content2" onclick="out()" id="out" >2</div> ';
