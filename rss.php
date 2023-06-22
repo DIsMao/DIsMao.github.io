490ad1a2d003331e8258f27987a26ef46c09deb5
DIsMao <74967959+DIsMao@users.noreply.github.com>
2023-06-22 14:55:46 +0300

    Add files via upload

---
+++ rss.php
@@ -0,0 +1,24 @@
+<rss version="2.0">
+	<channel>
+		<title>Название канала</title>
+        <description>Описание канала</description>
+        <link>https://dismao.github.io/</link>
+		<image>
+			<url>https://dismao.github.io/123.png</url>
+			<title>Название канала</title>
+			<link>https://dismao.github.io/</link>
+		</image>
+		<item>
+			<title>RSS Tutorial</title>
+			<link>https://dismao.github.io/</link>
+			<description>
+				<![CDATA[
+                <p>New RSS tutorial on Snipp.ru</p>
+				]]>
+			</description>
+			<category>PHP</category>
+			<guid>5</guid>
+			<pubDate>Thu, 08 Sep 16 17:00:00 +0300</pubDate>
+		</item>
+	</channel>        
+</rss>
